c04e9e45226fade1bdafbfb811dfe7bb7db15f47
Vijay Sharma <157729319+omvijaysharma@users.noreply.github.com>
2025-04-24 10:08:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,5 @@ function sendEmail() {
       const body = `Hello,%0D%0A%0D%0AI visited your portfolio website and wanted to get in touch with you.%0D%0A%0D%0AName: [Your Name]%0D%0AEmail: [Your Email]%0D%0AMessage:%0D%0A[Type your message here]%0D%0A%0D%0ARegards,%0D%0AA Visitor`;
       window.location.href = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${body}`;
     }
+
+document.addEventListener('contextmenu', event => event.preventDefault());
